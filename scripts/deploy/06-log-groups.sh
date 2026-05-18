867427bdf0f4b2978916ba05e92847992964ea1a
Simon Owusu <zx8086@mac.com>
2026-05-19 00:24:47 +0200

    SIO-789: phase A.4-A.6 deploy scripts for ECR, ECS cluster, log groups

---
+++ scripts/deploy/06-log-groups.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+SCRIPT_NAME="06-log-groups"
+source "$(dirname "$0")/lib.sh"
+
+ensure_log_group() {
+  local name="$1"
+  if aws logs describe-log-groups --log-group-name-prefix "$name" \
+    --query "logGroups[?logGroupName=='$name'] | length(@)" --output text | grep -q '^0$'; then
+    aws logs create-log-group --log-group-name "$name"
+    aws logs put-retention-policy --log-group-name "$name" --retention-in-days 7
+    log "created log group $name"
+  else
+    log "log group $name already exists"
+  fi
+}
+
+ensure_log_group /eventgate/gateway
+ensure_log_group /eventgate/writer
